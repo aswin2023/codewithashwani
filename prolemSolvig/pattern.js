fe9c3379dbf411a62a0e06b6563c8b366a65ed7c
Ashwani <126276564+aswin2023@users.noreply.github.com>
2023-10-16 02:16:29 +0530

    triangleLefttPattern

--- prolemSolvig/pattern.js
+++ prolemSolvig/pattern.js
@@ -51,3 +51,18 @@ function triangleRightPattern(number) {
     }
 }
 triangleRightPattern(5);
+
+function triangleLefttPattern(number) {
+    for (let i = 1; i <= number; i++) {
+        let stars = "";
+        for (let j = 1; j <= i; j++) {
+            /*
+            i=1,2,2,3,3,3,4,4,4,4,5,5,5,5,5
+            j=1,1,2,1,2,3,1,2,3,4,1,2,3,4,5
+            */
+            stars += "*"
+        }
+        console.log(stars)
+    }
+}
+triangleLefttPattern(5)
