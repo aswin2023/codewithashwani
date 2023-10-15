e57ca83fc697e8f8d2b6f0b63e9b94217fa5d984
Ashwani <126276564+aswin2023@users.noreply.github.com>
2023-10-16 02:12:44 +0530

    hollowSquarePattern

--- prolemSolvig/pattern.js
+++ prolemSolvig/pattern.js
@@ -12,3 +12,23 @@ function squareStarPatter(number) {
     }
 }
 squareStarPatter(5)
+
+function hollowSquarePattern(number) {
+    for (i = 1; i <= number; i++) {
+        let aswin = '';
+        for (j = 1; j <= number; j++) {
+            if (j === 1 || i === 1 || i === number || j === number) {
+                /*
+                i=1,1,1,1,1,2,2,3,3,4,4,5,5,5,5,5
+                j=1,2,3,4,5,1,5,1,5,1,5,1,2,3,4,5
+                */
+                aswin += '*'
+            }
+            else {
+                aswin += ' '
+            }
+        }
+        console.log(aswin)
+    }
+}
+hollowSquarePattern(5)
