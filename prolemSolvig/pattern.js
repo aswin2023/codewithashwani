dfd615c630b1ee6b57e71e5bac674d59467f56e0
Ashwani <126276564+aswin2023@users.noreply.github.com>
2023-10-16 05:00:04 +0530

    pascalLeftPattern

--- prolemSolvig/pattern.js
+++ prolemSolvig/pattern.js
@@ -295,7 +295,7 @@ function pyramidHollowDiamondPattern(number) {
 }
 pyramidHollowDiamondPattern(5);
 
-function rightPascalPattern(number) {
+function pascalRightPattern(number) {
     for (let i = 1; i <= number; i++) {
         let stars = "";
         for (let j = 1; j <= i; j++) {
@@ -320,4 +320,39 @@ function rightPascalPattern(number) {
         console.log(stars)
     }
 }
-rightPascalPattern(5)
+pascalRightPattern(5)
+
+function pascalLeftPattern(number) {
+    for (let i = 1; i <= number; i++) {
+        let aswin = '';
+        for (let j = 1; j <= number; j++) {
+            if (j <= number - i) {
+                aswin += ' ';
+            } else {
+                /*
+                i=1,2,2,3,3,3,4,4,4,4,5,5,5,5,5
+                j=5,4,5,3,4,5,2,3,4,5,1,2,3,4,5
+                */
+                aswin += '*';
+            }
+        }
+        console.log(aswin);
+    }
+
+    for (let i = number - 1; i >= 1; i--) {
+        let aswin = '';
+        for (let j = 1; j <= number; j++) {
+            if (j <= number - i) {
+                aswin += ' ';
+            } else {
+                /*
+                i=
+                j=
+                */
+                aswin += '*';
+            }
+        }
+        console.log(aswin);
+    }
+}
+pascalLeftPattern(5);
